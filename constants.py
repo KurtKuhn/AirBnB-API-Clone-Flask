7bf2e649b386f8b345b1664c08f0d361cd8d3300
KurtKuhn <kuhnku@oregonstate.edu>
2021-05-29 15:23:35 -0700

    Attempting Google OAuth

---
+++ constants.py
@@ -0,0 +1,6 @@
+
+
+# Google API OAUth Stuff
+CLIENT_ID = '885754310884-725vsgunbh9ohfmpuji6eb9qr834kleg.apps.googleusercontent.com'
+CLIENT_SECRET = 'krduD0MFgBj8KJ9vzVH8iOpX'
+URI = "http://localhost:5000/"
